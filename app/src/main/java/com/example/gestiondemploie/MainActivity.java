6dd192cd697539a3c7cd4e2cad4a4f79c0f04c9c
Ruben Illoki <rubenilloki92@gmail.com>
2021-03-11 03:16:44 +0100

    premier commit sur la connexion la presentation et les offres

---
+++ app/src/main/java/com/example/gestiondemploie/MainActivity.java
@@ -0,0 +1,33 @@
+package com.example.gestiondemploie;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+    private EditText txtLogin, txtPassword;
+    private Button btnConnect, btnSignUp;
+    private String login, password;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        txtLogin=findViewById(R.id.txtLogin);
+        txtPassword=findViewById(R.id.txtPassword);
+        btnConnect=findViewById(R.id.bntConnect);
+        btnSignUp=findViewById(R.id.bntSignUp);
+        btnConnect.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, HomeActivity.class);
+                startActivity(intent);
+            }
+        });
+
+    }
+}
---
+++ app/src/main/java/com/example/gestiondemploie/OfferFragment.java
@@ -0,0 +1,66 @@
+package com.example.gestiondemploie;
+
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+
+public class OfferFragment extends Fragment {
+    private ListView listOffer;
+    private String [] tabOffer,tabDetails;
+    private String  details;
+    public static String offer;
+
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.fragment_offer, container, false);
+        listOffer = view.findViewById(R.id.listOffer);//Recuperation de la liste
+        tabOffer = getResources().getStringArray(R.array.tab_Offer);
+        tabDetails = getResources().getStringArray(R.array.tab_Offer);
+
+        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<>(getActivity(),android.R.layout.simple_list_item_1,tabOffer);
+        listOffer.setAdapter(arrayAdapter);//chargement des donnees
+        listOffer.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                offer = tabOffer [position];
+                details = tabDetails [position];
+
+                AlertDialog.Builder dialog = new AlertDialog.Builder(getActivity());//builder parceque ya deux button
+                dialog.setIcon(R.mipmap.ic_launcher);
+                dialog.setTitle(offer);
+                dialog.setMessage(details);
+                dialog.setNegativeButton(getString(R.string.cancel), null);
+                dialog.setPositiveButton(getString(R.string.apply), new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        //appel du fragment apply(postuler)
+
+                    }
+                });
+                 dialog.show();//
+
+
+
+
+
+
+            }
+        });
+
+        return view;
+    }
+}
